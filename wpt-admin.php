7f0402953938bc2b42330c2cf76d028d221528cf
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-17 13:00:03 +0000

    Initial Upload 1.0
    git-svn-id: https://plugins.svn.wordpress.org/pricing-tables/trunk@916103 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpt-admin.php
@@ -0,0 +1,9 @@
+<div class="wrap">
+	<?php echo "<h2>".__('Wordpress Pricing Table Info')."</h2>";?>
+    <br />
+
+		<strong>Please send us feedback</strong>
+		<div>if you need more help please feel free to join our forum and ask any question<br />
+<a href="http://kentothemes.com/support/" >KentoThemes.com Questions & Answers</a>
+		</div>   
+</div>
