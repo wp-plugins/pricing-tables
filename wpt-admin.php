ef499b2e6ec6d73637a869f36873906b84d8082e
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-31 09:48:50 +0000

    upload 1.3
    git-svn-id: https://plugins.svn.wordpress.org/pricing-tables/tags/1.3@924175 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpt-admin.php
+++ wpt-admin.php
@@ -14,6 +14,39 @@ Please watch this video tutorial<br /> <br />
 Please report any issue via our support forum <a href="http://kentothemes.com/questions-answers/">kentothemes.com &raquo; Q&A</a> ask any question. <br />
         Checkout Our Latest Plugin <a href="http://kentothemes.com/">http://kentothemes.com</a>
 </p>
+<br />
+<hr />
+<h3>Do You Like This Plugin ?</h3>
+<table>
+<tr>
+<td width="100px"> 
+<!-- Place this tag in your head or just before your close body tag. -->
+<script type="text/javascript" src="https://apis.google.com/js/platform.js"></script>
+
+<!-- Place this tag where you want the +1 button to render. -->
+<div class="g-plusone" data-size="medium" data-href="http://kentothemes.com/items/plugins/wordpress-pricing-table/"></div>
+
+</td>
+<td width="100px">
+
+<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fkentothemes.com%2Fitems%2Fplugins%2Fwordpress-pricing-table%2F&amp;width=100&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=743541755673761" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
+
+ </td>
+<td width="100px"> 
+
+
+
+
+
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://kentothemes.com/items/plugins/wordpress-pricing-table/" data-text=" WordPress Pricing Tables">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</td>
+
+</tr>
+
+</table>
+<hr />
+
 
 <br />
 <h3>Need Premium Version Of "Pricing Tables" ?</h3>
@@ -27,7 +60,11 @@ If you need more feature for your pricing table then you could try our pro versi
 # One Time purchase.<br />
 # 7 Days Refund.<br />
 # Easy to use.<br />
-# Four different themes.<br />
+# Different Background Color for column.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Four different themes.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Top And Bottom Gradient. <strong style="color:#139b50;">(Premium Only)</strong><br />
+# Column Corner Radius.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Table Column Margin.<strong style="color:#139b50;">(Premium Only)</strong><br />
 # Hide Blank Field Style. <strong style="color:#139b50;">(Premium Only)</strong><br />
 # Custom font size for column header, price, signup link.<strong style="color:#139b50;">(Premium Only)</strong><br />
 # Custom color for column header, price, signup link.<strong style="color:#139b50;">(Premium Only)</strong><br />
